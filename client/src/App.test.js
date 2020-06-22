7ef306deadd20469a2aa3c63ec00f02d14be07d4
Ferran <ferran@rebellionpay.com>
2020-06-22 16:35:09 +0200

    test deleted

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  console.log("eii")
-  ReactDOM.unmountComponentAtNode(div);
+ 
 });
